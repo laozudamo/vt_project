5731f57dabb1295b0c87a37439c5821b94cc967e
laozudamo <laozudamo@gmail.com>
2022-05-13 00:10:00 +0800

    feat(project): 环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), vueJsx()],
+  base: './',    // 开发环境
+  resolve: {
+    alias: [
+      {
+        find: '@/',
+        replacement: join(__dirname, "src/")
+      }
+    ]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/styles/variable.scss';`
+      }
+    }
+  },
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment'
+  }
 })
